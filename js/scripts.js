5927b68182621f417b4bb93e13e892f619ff27fd
Gennaro Anzalone and Mitch Long <gennaro@>
2017-08-07 15:13:13 -0700

    add jquery click commands to cypher

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,18 @@ var bookendCapitalize = function(inputSentence){
   var capitalizeFirstLetter = inputSentence.charAt(0).toUpperCase();
   var length = inputSentence.length;
   var capitalizeLastLetter = inputSentence.charAt(length-1).toUpperCase();
-
   return capitalizeFirstLetter + capitalizeLastLetter; //outputs IE
 }
 
-var invert = function(sentence){
-  return sentence.split("").reverse().join("");
+var reverseCapitalize = function (inputSentence){
+  var capitalizeFirstLetter = inputSentence.charAt(0).toUpperCase();
+  var length = inputSentence.length
+  var capitalizeLastLetter = inputSentence.charAt(length-1).toUpperCase();
+  return capitalizeLastLetter + capitalizeFirstLetter;  //outputs EI
+}
+
+var invert = function(sentence){ //input "hello"
+  return sentence.split("").reverse().join(""); //outputs "olleh"
 };
 
 var capitalizeInvert = function(sentence){ //With input "I am a sentence"
@@ -19,19 +25,16 @@ var findMiddle = function(sentence){
   return sentence.charAt(middle) +capitalizeInvert(sentence); //outputs sI am a sentenceEI
 }
 
-var totalReverse = function (sentence){
-
-}
-
-var reverseCapitalize = function (inputSentence){
-  var capitalizeFirstLetter = inputSentence.charAt(0).toUpperCase();
-  var length = inputSentence.length
-  var capitalizeLastLetter = inputSentence.charAt(length-1).toUpperCase();
-
-  return capitalizeLastLetter + capitalizeFirstLetter;  //outputs EI
-}
-
-
-var sentence = prompt("Please enter a sentence!"); //use input "I am a sentence"
+var sentence = prompt("What is your favorite Sentence."); //use input "I am a sentence"
 var result = findMiddle(sentence);
 alert(result);
+
+$(document).ready(function() {
+  $("img").click(function() {
+    alert(sentence);
+  });
+
+  $("h1").click(function(){
+    alert(findMiddle(sentence));
+  });
+});
